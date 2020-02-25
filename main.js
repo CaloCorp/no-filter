c1d69020fad47dc3680a9bf56adfb1056e63b080
Calo Corp <marc.calo@codeimmersives.com>
2020-02-25 15:23:30 -0500

    complete with mastery

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function onlyOdds (array){
+const oddNumbers=[]
+  for(let i=0;i <array.length; i++) {
+    const numberInTheList=array[i]
+  
+    if(numberInTheList % 2 !== 0) {
+      oddNumbers.push(numberInTheList)
+    }
+  }
+
+return oddNumbers
+
+}
 
 
+function onlyEvens (array){
+  const evenNumbers=[]
+    for(let i=0;i <array.length; i++) {
+      const numberInTheList=array[i]
+    
+      if(numberInTheList % 2 === 0) {
+        evenNumbers.push(numberInTheList)
+      }
+    }
+    
+  return evenNumbers
+  
+  }
 
+function shortNamesOnly(names){
+const theShortNames=[];
 
+for (let i=0;i<names.length;i++){
+const namesInTheArray=names[i]
 
+if(namesInTheArray.length < 7){
 
+  theShortNames.push(namesInTheArray)
+}
+
+}
+return theShortNames;
+}
+
+function dNames (array){
+const theDnames=[]
+
+for (let i=0;i<array.length;i++){
+const theListOfNamesAgain =array[i]
+if(theListOfNamesAgain.startsWith('D')){
+
+theDnames.push(theListOfNamesAgain)
+}
+}
+return theDnames;
+}
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
